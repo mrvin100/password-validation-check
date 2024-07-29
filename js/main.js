d02b9aecb8b5991bcee892036fe263209e5417e8
vincent youmssi <vincentyoumssi@gmail.com>
2024-07-29 10:19:59 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+/* custom js file made by JeanDoe */
+
+const passwordInput = document.querySelector(".pass-field input"),
+	  eyeIcon = document.querySelector(".pass-field i"),
+	  requirementList = document.querySelectorAll(".requirement-list li");
+
+// An array password requirements with corresponding
+// regular expressions and index of the requirement list item
+const requirements = [
+	{ regex: /.{8,}/, index: 0}, // Minimum of 8 characters
+	{ regex: /[0-9]/, index: 1}, // At least one number
+	{ regex: /[a-z]/, index: 2}, // At least one lowercase letter
+	{ regex: /[^A-Za-z0-9]/, index: 3}, // At least one special character
+	{ regex: /[A-Z]/, index: 4}, // At least one uppercase letter
+]
+
+passwordInput.addEventListener('keyup', (e) => {
+	requirements.forEach(item => {
+		// Check if the password matches the requirement regex
+		const isValid = item.regex.test(e.target.value);
+		const requirementItem = requirementList[item.index];
+		
+		// Updating class and icon of requirement item if requirement matched or not
+		
+		if(isValid){
+			requirementItem.classList.add("valid");
+			requirementItem.firstElementChild.className = "fas fa-check";
+		}else{
+			requirementItem.classList.remove("valid");
+			requirementItem.firstElementChild.className = "fas fa-circle";
+		}
+	});
+});
+
+eyeIcon.addEventListener("click", () => {
+	// Toggle the password input type between 'password' and 'text'
+	passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+	
+	// Update the eye icon class based on the password input type
+	eyeIcon.className = `fas fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
+});
+
+
+
+
+
+
+/* show or Hide content box */
+const content = document.querySelector('.content'),
+	  helpBtn = document.getElementById('modal');
+
+if(helpBtn){
+	helpBtn.addEventListener('click', () => {
+		content.classList.toggle('active');
+	});
+}
+
+
+/* show or Hide user Guide Message box */
+
+const userGuide = document.querySelector('.view'),
+	  text = document.querySelector('.text'),
+	  closeContent = document.querySelector('.close');
+
+if(userGuide){
+	userGuide.addEventListener('click', () =>{
+		text.classList.add('active');
+		closeContent.classList.add('active');
+	});
+}
+
+if(closeContent){
+	closeContent.addEventListener('click', () =>{
+		text.classList.remove('active');
+		closeContent.classList.remove('active');
+	});
+}
